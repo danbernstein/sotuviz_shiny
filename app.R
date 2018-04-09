188c4a9192278aefbc49042255200ad2e076d26e
danbernstein <danbernstein94@gmail.com>
2018-04-09 00:15:50 -0400

    apr 9.1

--- app.R
+++ app.R
@@ -15,22 +15,45 @@ plot_data <- read.csv("https://raw.githubusercontent.com/danbernstein/sotuviz_sh
 
 tidy.count <- read.csv("https://raw.githubusercontent.com/danbernstein/sotuviz_shiny/master/data/tidy_countbypresidentyear.csv")
 
+topic.words <- read.csv("https://raw.githubusercontent.com/danbernstein/sotuviz_shiny/master/data/topicmodelling_logratio_topwordspertopic.csv")
+
+ay <- list(
+  tickfont = list(color = "black"),
+  overlaying = "y",
+  side = "right",
+  title = "Estimated Term Frequency",
+  showgrid = FALSE,
+  showline = FALSE
+)
+
 # Define UI for application that draws a histogram
 ui <- shinyUI(navbarPage("Trend Analysis of State of the Union Addresses",
-    # Set theme
-    #theme = shinytheme("spacelab"),
-#   h2("Trend Analysis of State of the Union Addresses"),
-#   h4("Investigating linguistic changes from Washington to Obama using text mining tools"),
-#   
-#   # Vertical space
-#   tags$hr(),
-    
+                         
         tabPanel("Topic Modelling",
                  sidebarLayout(
-                   sidebarPanel("Topic Modelling is an unsupervised learning technique that models each document in a corpus as a mixture of topics, and each topic as a mixture of words. 
-                                At the document level, the output for each document is represented as a percent of each topic, ranging from 0 to 1."),
-                mainPanel(plotlyOutput(outputId = "timeseries", width = "100%")))
-        ),
+                   sidebarPanel(
+                               selectizeInput(inputId = "plot_word", 
+                                           label = "Choose from the most unique terms in each topic", 
+                                           choices = 
+                                             stringr::str_to_title(as.list(topic.words)),
+                                           options = list(
+                                             placeholder = 'Please select an option below',
+                                             onInitialize = I('function() { this.setValue(""); }')
+                                           )),
+                               textInput("text_input1", "or enter another word",
+                                           placeholder = 'enter another word to overlay'
+                                         )
+                               ),
+                mainPanel(plotlyOutput(outputId = "timeseries", width = "95%"))),
+                tags$hr(),
+                tags$blockquote(
+                  div(
+                    p(HTML("Topic Modelling is an unsupervised learning technique that models each document in a corpus as a mixture of topics, and each topic as a mixture of words. 
+                           At the document level, the output for each document is represented as a percent of each topic, ranging from 0 to 1. The addresses have been partitioned into three 
+                           topics using the Latent Dirichlet Allocation (LDA) model, where the document is made up of a proportion of each topic, and each topic is a collection of terms, which can 
+                           contribute to one or more topics. To better understand how terms can contribute to more than one topic, you can use the sidepanel to overlay the term trend usage, generated as a smoothed spline."))))
+                ),
+        
         tabPanel("Text Similarity",
                  sidebarLayout(
                   sidebarPanel(
@@ -38,8 +61,7 @@ ui <- shinyUI(navbarPage("Trend Analysis of State of the Union Addresses",
                              c("Cosine" = "Cosine Function",
                                "Correlation" = "Correlation",
                                "Jaccard" = "Jaccard"), 
-                             "Cosine Function", inline = TRUE),
-                  verbatimTextOutput(outputId = "heatmap_topvalues")
+                             "Cosine Function", inline = TRUE)
                   ),
               mainPanel(plotlyOutput(outputId = "plot", height = "550px"))),
        tags$hr(),
@@ -57,21 +79,74 @@ ui <- shinyUI(navbarPage("Trend Analysis of State of the Union Addresses",
 
 
 # Define server logic required to draw a histogram
+
 server <- function(input, output) {
-   
-   output$timeseries <- renderPlotly({
-     p <- plot_ly(source = "source") %>% 
-         add_lines(data = plot_data, x = ~year, y = ~round(Topic_1, 3), name = "Topic 1", mode = "lines", line = list(width = 3)) %>% 
-         add_lines(data = plot_data, x = ~year, y = ~round(Topic_2, 3), name = "Topic 2", mode = "lines", line = list(width = 3)) %>% 
-         add_lines(data = plot_data, x = ~year, y = ~round(Topic_3, 3), name = "Topic 3",mode = "lines", line = list(width = 3)) %>% 
-       layout(hoverlabel= list(opacity = 0.9),
-              title = '',
-              yaxis = list(title = 'Topic Percentage'),
-              xaxis = list(title = 'Year'),
-              showlegend = F)
-     
-     p
-   })
+  
+  word.df <- reactive({
+    ok <- tidy.count %>% 
+      filter(word == tolower(input$text_input1)) 
+  })
+  
+  dropdown.word.df <- reactive({
+    ok <- tidy.count %>% 
+      filter(word == tolower(input$plot_word)) 
+  })
+  
+
+  output$timeseries <- renderPlotly({
+     if (input$text_input1 != "" & input$plot_word == "") 
+       {
+     overlay_word.df <- word.df()
+     overlay_word.smooth <- ksmooth(overlay_word.df$year, overlay_word.df$n, "normal", bandwidth = 20)
+ 
+         p <- plot_ly(source = "source") %>% 
+         add_lines(data = overlay_word.smooth, x = ~round(x), y = ~y, yaxis = "y2", name = input$text_input1, line = list(shape = "spline")) %>% 
+           layout(
+             yaxis2 = ay
+           )
+    } else if(input$text_input1 != "" & input$plot_word != "")
+        {
+        overlay_word.df <- word.df()
+        overlay_word.smooth <- ksmooth(overlay_word.df$year, overlay_word.df$n, "normal", bandwidth = 20)
+        
+        overlay_chosenword.df <- dropdown.word.df()
+        overlay_chosenword.smooth <- ksmooth(overlay_chosenword.df$year, overlay_chosenword.df$n, "normal", bandwidth = 20)
+        
+        p <- plot_ly(source = "source") %>% 
+          add_lines(data = overlay_word.smooth, x = ~round(x), y = ~y, yaxis = "y2", name = input$text_input1, line = list(shape = "spline")) %>% 
+          add_lines(data = overlay_chosenword.smooth, x = ~round(x), y = ~y, yaxis = "y2", name = input$plot_word, line = list(shape = "spline")) %>% 
+          layout(
+            yaxis2 = ay
+          )
+      }
+    else if(input$text_input1 == "" & input$plot_word != "")
+    {
+      overlay_chosenword.df <- dropdown.word.df()
+      overlay_chosenword.smooth <- ksmooth(overlay_chosenword.df$year, overlay_chosenword.df$n, "normal", bandwidth = 20)
+      
+      p <- plot_ly(source = "source") %>% 
+        add_lines(data = overlay_chosenword.smooth, x = ~round(x), y = ~y, yaxis = "y2", name = input$plot_word, line = list(shape = "spline")) %>% 
+        layout(
+          yaxis2 = ay
+        )
+    }else{
+      p <- plot_ly(source = "source") 
+    }
+
+  p %>% 
+    add_lines(data = plot_data, x = ~year, y = ~round(Topic_1, 3), name = "Topic 1", mode = "lines", line = list(width = 3), showlegend = F) %>% 
+    add_lines(data = plot_data, x = ~year, y = ~round(Topic_2, 3), name = "Topic 2", mode = "lines", line = list(width = 3), showlegend = F) %>% 
+    add_lines(data = plot_data, x = ~year, y = ~round(Topic_3, 3), name = "Topic 3",mode = "lines", line = list(width = 3), showlegend = F) %>% 
+    layout(hoverlabel= list(opacity = 0.9),
+           title = '',
+           yaxis = list(title = 'Topic Percentage'),
+           xaxis = list(title = 'Year'),
+           showlegend = T,
+           margin = list(l = 50, r = 50, b = 50, t = 50, pad = 4),
+           legend = list(x = 105, y = 1.05))
+  
+    
+ })
    
    heatmap.df <- reactive({
      switch(input$simil_type,
@@ -126,15 +201,15 @@ server <- function(input, output) {
               xaxis = list(title = "", nticks = 10),
               yaxis = list(title = "", autorange = "reversed"), autosize = T) 
    })
-   output$heatmap_topvalues <- renderText({
-     
-     eventdata <- event_data("plotly_click", source = "source")
-     validate(need(!is.null(eventdata), "Hover over the heatmap to see the president comparisons"))
-     
-     datapoint <- as.numeric(eventdata$x)[1]
-     
-     datapoint
-   })
+ #  output$heatmap_topvalues <- renderText({
+ #    
+ #    eventdata <- event_data("plotly_click", source = "source")
+ #    validate(need(!is.null(eventdata), "Hover over the heatmap to see the president comparisons"))
+ #    
+ #    datapoint <- as.numeric(eventdata$x)[1]
+ #    
+ #    datapoint
+ #  })
      
 }
 
